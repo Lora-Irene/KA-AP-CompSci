257d0debd3f5615aa2ea309623fcf7252ba716df
Lora-Irene <60989056+Lora-Irene@users.noreply.github.com>
2020-02-13 17:18:32 -0800

    fixed in JS.do
    Added <br> HTML line breaks
    changed println to document.write
    added var data

--- 2020.2.13 Compound Booleans Weather.js
+++ 2020.2.13 Compound Booleans Weather.js
@@ -1,19 +1,19 @@
 //set variables for what it's like outside
-var temperature
-var weather
-var transportMode
+var temperature = 72
+var weather = "sunny"
+var transportMode = "walking"
 
 //based on weather and temperature should I wear a jacket?
 if (temperature < 40 || weather === "rain") {
-    println ("Wear a jacket!");
+    document.write ("Wear a jacket!<br><br>");
 }
 
 //based on weather and mode of transportation do I need an umbrella?
 if (weather === "rain" &&
 transportMode === "walking") {
-    document.write ("Take an umbrella");
+    document.write ("Take an umbrella<br><br>");
 }
 // Is it a gardening day?
 if (!(temperature < 70 || weather === "rain")) {
-    document.write ("Gardening day!");
+    document.write ("Gardening day!<br><br>");
 }
