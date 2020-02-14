b591edb34ba346e220bfb2f815a53872dae524dc
Lora-Irene <60989056+Lora-Irene@users.noreply.github.com>
2020-02-13 16:14:35 -0800

    Update 2020.2.13 Compound Booleans Graduation.js
    Added new language requirement to graduate

--- 2020.2.13 Compound Booleans Graduation.js
+++ 2020.2.13 Compound Booleans Graduation.js
@@ -1,4 +1,6 @@
-if (CumulativeGPA > 2.0 && totalUnits >= 120) {
+if (CumulativeGPA > 2.0 && 
+    totalUnits >= 120 && 
+    languageUnits >= 8) {
         document.write ("You can graduate!")
     }
 }
